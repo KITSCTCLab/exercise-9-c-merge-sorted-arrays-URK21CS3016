2de3ce05864f57d92a8c3df69af6f22c57828dba
URK21CS3016 <109951881+URK21CS3016@users.noreply.github.com>
2022-10-09 21:55:21 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,26 @@
 from typing import List
+def partition(array, low, high):
+    pivot = array[high]
+    i = low - 1
+    for j in range(low, high):
+        if array[j] <= pivot:
+            i = i + 1
+            (array[i], array[j]) = (array[j], array[i])
+    (array[i + 1], array[high]) = (array[high], array[i + 1])
+    return i + 1
+def quick_sort(data, low, high) -> List[int]:
+    if low < high:
+        pi = partition(array, low, high)
+        quick_sort(array, low, pi - 1)
+        quick_sort(array, pi + 1, high)
+    return array
 
-def merge(nums1: List[int], m: int, nums2: List[int], n: int) -> None:
-  # Write code here
 
-
-# Do not change the following code
-nums1 = []
-nums2 = []
-for item in input().split(', '):
-  nums1.append(int(item))
-for item in input().split(', '):
-  nums2.append(int(item))
-m = int(input())
-n = int(input())
-merge(nums1, m, nums2, n)
-print(nums1)
+input_data = input()
+data = []
+for item in input_data.split(', '):
+  if item.isnumeric():
+    data.append(int(item))
+  elif item.lstrip("-").isnumeric():
+    data.append(int(item))
+print(quick_sort(data, 0, len(data)-1))
